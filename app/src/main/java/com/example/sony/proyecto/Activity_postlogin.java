1f5795bb037cfb603662f0638d800845d4dded76
SONY VAIO <SONY VAIO>
2017-05-10 00:19:57 -0500

    First commit

--- app/src/main/java/com/example/sony/proyecto/Activity_postlogin.java
+++ app/src/main/java/com/example/sony/proyecto/Activity_postlogin.java
@@ -27,14 +27,14 @@ public class Activity_postlogin extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         //Animacion de fondo
 
